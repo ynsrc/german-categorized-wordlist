370b1923751c436b1f0a99532d372706aa584349
ynsrc <48865360+ynsource@users.noreply.github.com>
2024-05-19 02:23:37 +0300

    update wkt-word-parser tool

--- tools/wkt-wordparser/wkt-word-parser.go
+++ tools/wkt-wordparser/wkt-word-parser.go
@@ -5,8 +5,6 @@
 ** Such as single_output.txt explained in README.md file at repository root.
  */
 
-// TODO: This script is incomplete and non-functional now.
-
 package main
 
 import (
@@ -14,40 +12,127 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
 	s "strings"
 )
 
+const failIfNotParsed = false
+const columnSeperator = "; "
+
+var wordTypesPattern = regexp.MustCompile("=== (.*?\\|Deutsch}}.*?) ===")
+
+func parseWordType(raw string) string {
+	raw = s.Trim(raw, " \r\r\n")
+
+	if s.HasPrefix(raw, "{{Wortart|") {
+		return s.Split(raw, "|")[1]
+	} else if s.HasPrefix(raw, "{{") {
+		raw = s.Replace(raw, "{{", "", -1)
+		raw = s.Replace(raw, ".}}", "", -1)
+		raw = s.Replace(raw, "}}", "", -1)
+		return raw
+	} else if s.HasPrefix(raw, "''") {
+		return s.Replace(raw, "'", "", -1)
+	}
+
+	return raw
+}
+
+func parseWord(word string, text string) string {
+	var detectedWordTypes []string
+
+	if wordTypesMatches := wordTypesPattern.FindAllStringSubmatch(text, 1); len(wordTypesMatches) > 0 {
+		wordTypes := wordTypesMatches[0][1]
+
+		if s.Contains(wordTypes, ", ") {
+			for _, wordTypeRaw := range s.Split(wordTypes, ", ") {
+				wordType := parseWordType(wordTypeRaw)
+				if wordType != "" {
+					detectedWordTypes = append(detectedWordTypes, wordType)
+				} else if failIfNotParsed {
+					panic("Undetectable word type: " + wordTypeRaw + " for " + word)
+				} else {
+					fmt.Fprintln(os.Stderr, "Undetectable word type: "+wordTypeRaw+" for "+word)
+				}
+			}
+		} else {
+			wordType := parseWordType(wordTypes)
+			if wordType != "" {
+				detectedWordTypes = append(detectedWordTypes, wordType)
+			} else if failIfNotParsed {
+				panic("Undetectable word type: " + wordTypes + " for " + word)
+			} else {
+				fmt.Fprintln(os.Stderr, "Undetectable word type: "+wordTypes+" for "+word)
+			}
+		}
+	}
+
+	if len(detectedWordTypes) > 0 {
+		return word + columnSeperator + s.Join(detectedWordTypes, "+")
+	} else if failIfNotParsed {
+		panic("No word type found for: " + word)
+	} else {
+		fmt.Fprintln(os.Stderr, "No word type found for: "+word)
+	}
+
+	return ""
+}
+
 func main() {
-	var fileName string
-	flag.StringVar(&fileName, "i", "single_output.txt", "input file to parse")
+	var inputFileName string
+	flag.StringVar(&inputFileName, "i", "single_output.txt", "input file to parse")
 
-	fmt.Fprintf(os.Stderr, "Parsing file: %s", fileName)
+	var outputFileName string
+	flag.StringVar(&outputFileName, "o", "parsed_words.csv", "target csv file for parsed words")
 
-	file, err := os.Open(fileName)
+	fmt.Fprintln(os.Stderr, "Parsing file:", inputFileName)
+	fmt.Fprintln(os.Stderr, "Output file:", outputFileName)
+
+	inputFile, err := os.Open(inputFileName)
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
+	defer inputFile.Close()
 
-	scanner := bufio.NewScanner(file)
+	outputFile, err := os.Create(outputFileName)
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	scanner := bufio.NewScanner(inputFile)
 
 	current_word := ""
+	current_text := ""
+
+	fmt.Fprintln(os.Stderr, "Parsing words, please wait...")
 
 	for scanner.Scan() {
-		line := s.Trim(scanner.Text(), " \r\n\t")
-
-		if len(line) == 0 {
-			continue
-		}
+		line := scanner.Text()
 
 		if s.HasPrefix(line, "/*") {
+			if current_word != "" && current_text != "" {
+				if csv_line := parseWord(current_word, current_text); csv_line != "" {
+					outputFile.WriteString(csv_line + "\n")
+				}
+			}
+
 			word := line[s.Index(line, "title: ")+7 : s.Index(line, ", id: ")]
-			// finish previous word
 			current_word = word
-			fmt.Println(current_word)
+			current_text = ""
+
 			continue
+		} else {
+			current_text += line + "\n"
 		}
 	}
+
+	// process last word
+	if current_word != "" && current_text != "" {
+		parseWord(current_word, current_text)
+	}
+
+	fmt.Fprintln(os.Stderr, "Completed!")
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "Error:", err)
