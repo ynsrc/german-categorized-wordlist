d01a9601aac172528fc4a5dfc530b642196d83ee
ynsrc <48865360+ynsource@users.noreply.github.com>
2024-05-19 21:27:43 +0300

    fix word type parsing and update results

--- tools/wkt-wordparser/wkt-word-parser.go
+++ tools/wkt-wordparser/wkt-word-parser.go
@@ -21,7 +21,7 @@ const columnSeperator = "; "
 const wordTypeListSeperator = "+"
 
 var wordTypesPattern = regexp.MustCompile("=== (.*?\\|Deutsch}}.*?) ===")
-var typeListPattern = regexp.MustCompile("[\\{'\\w]{1,2}[^}']+[}'\\w]{1,2}")
+var typeListPattern = regexp.MustCompile("[\\{'\\w]{1,2}[^}{']+[}'\\w]{1,2}")
 
 func parseWordTypes(raw string) []string {
 	var detectedWordTypes []string
@@ -43,6 +43,8 @@ func parseWordTypes(raw string) []string {
 		} else {
 			detectedWordTypes = append(detectedWordTypes, raw)
 		}
+	} else if raw != "" {
+		detectedWordTypes = append(detectedWordTypes, raw)
 	}
 
 	return detectedWordTypes
