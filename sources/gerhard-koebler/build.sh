3a186a44a0f50917d6d41635e2150ccfd5074f78
ynsrc <48865360+ynsource@users.noreply.github.com>
2024-05-24 04:25:14 +0300

    add generated but unedited wordlists

--- sources/gerhard-koebler/build.sh
+++ sources/gerhard-koebler/build.sh
@@ -19,4 +19,31 @@ fi
 
 cd build
 
-cat grimm1.txt grimm2.txt edel.txt | sort -u > ../merged-wordlist.txt
+if ! [ -f merged-wordlist.txt ]; then
+    cat grimm1.txt grimm2.txt edel.txt | sort -u > ../merged-wordlist.txt
+fi
+
+cd ..
+
+TARGET=generated
+mkdir -p ${TARGET}
+rm -f ${TARGET}/*.txt
+
+grep '; Adj.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/adjective.txt
+grep '; Adv.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/adverb.txt
+grep '; Art.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/article.txt
+grep '; F.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/noun-die.txt
+grep '; Interj.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/interjection.txt
+grep '; Konj.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/conjunction.txt
+grep '; M.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/noun-der.txt
+grep '; N.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/noun-das.txt
+grep '; Num. Kard.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/number-cardinal.txt
+grep '; Num. Ord.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/number-ordinal.txt
+grep '; Partikel' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/particle.txt
+grep '; Pl.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/noun-plural.txt
+grep '; [Pp]räf.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/affix-prefix.txt
+grep '; Präp.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/preposition.txt
+grep '; Pron.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/pronoun.txt
+grep '; Sb.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/substantive.txt
+grep '; Suff.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/suffix.txt
+grep '; V.' merged-wordlist.txt | cut -d';' -f1 > ${TARGET}/verb.txt
