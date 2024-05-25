5d86f99f705608ab75e9e6ad56b0582cfb751d01
ynsrc <48865360+ynsource@users.noreply.github.com>
2024-05-25 19:57:25 +0300

    update build script and rebuild wordlists

--- build.sh
+++ build.sh
@@ -18,7 +18,56 @@ done
 for wlfile in $(echo build/*.txt);
 do
 	filename=$(basename $wlfile)
-	cat $wlfile | sort -u > "${TARGET}/$filename"
+
+	case $filename in
+		adjective.txt | adverb.txt | article.txt)
+			cat $wlfile | grep -vE '[…’\.\(]' | sort -u > "${TARGET}/$filename"
+		;;
+
+		affix-prefix.txt)
+			cat $wlfile | grep -E '^.*-$' | sort -u > "${TARGET}/$filename"
+		;;
+
+		affix-suffix.txt)
+			cat $wlfile | grep -E '^-' | tr '.…' '-' | tr -s '-' | grep '^-' | sort -u > "${TARGET}/$filename"
+		;;
+
+		comparative.txt)
+			cat $wlfile | grep -vE '^-' | sort -u > "${TARGET}/$filename"
+		;;
+
+		expression-multi-word.txt)
+			cat $wlfile | grep -v 'à' | sort -u > "${TARGET}/$filename"
+		;;
+
+		interjection.txt)
+			cat $wlfile | grep -vE '[à’,]' | tr -d '!' | sort -u > "${TARGET}/$filename"
+		;;
+
+		noun-das.txt | noun-der.txt | noun-die.txt)
+			cat $wlfile | grep -vE '^\.|[…\*\+\ǃ’„“§]' | sort -u > "${TARGET}/$filename"
+		;;
+
+		noun-plural.txt)
+			cat $wlfile | grep -vE '…|\(' | sort -u > "${TARGET}/$filename"
+		;;
+
+		number-cardinal.txt | number-ordinal.txt)
+			cat $wlfile | grep -vE '^\.|…' | sort -u > "${TARGET}/$filename"
+		;;
+
+		particle.txt)
+			cat $wlfile | grep -vE '^\.|[\.-]$|[…à]' | sort -u > "${TARGET}/$filename"
+		;;
+
+		verb.txt)
+			cat $wlfile | grep -vE '^\.|…' | tr -d '()' | sort -u > "${TARGET}/$filename"
+		;;
+
+		*)
+			cat $wlfile | sort -u > "${TARGET}/$filename"
+		;;
+	esac 
 done
 
 tar cvzf build/wordlists.tar.gz v1/*.txt README.md LICENSE 
